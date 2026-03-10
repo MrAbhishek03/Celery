82345305df954173c8ef9f4acfce2dac1a668256
Abhishek <mrabhishekpr17@gmail.com>
2026-03-10 08:01:07 +0530

    Updated the View home page

--- Api/views.py
+++ Api/views.py
@@ -8,17 +8,14 @@ from .tasks import add,sub
 
 def index(request):
     result1=add.apply_async(args=[10,2])
-    # result2=sub.apply_async(args=[40,36])
+    
     print("-*"*20)
     print(f"results page: {result1}")
     print(f"results state: {result1.state}")
     print(f"results status: {result1.status}")
     print("-*"*20)
 
-    # print(f"results2 page: {result2}")
-    # print(f"results2 state: {result2.state}")
-    # print(f"results2 status: {result2.status}")
-    # print("-*"*20)
+    
     return render(request, 'home.html', {'result1': result1, 'result2': result2} )
 
 def about(request):
